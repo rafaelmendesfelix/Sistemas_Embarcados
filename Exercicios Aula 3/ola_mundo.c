ed109ed16bc534d2b6a3bc0291f346b42faf0a95
rafaelmendesfelix <rafaelmendesf@hotmail.com>
2018-03-14 17:41:05 -0300

    Add files via upload

---
+++ Exercicios Aula 3/ola_mundo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[])	{
+
+char nome [30];
+	
+	printf("Insira o seu nome \n");
+	scanf ("%s",nome);
+	printf("olá %s \n",nome);
+	
+  
+return 0;
+
+
+					}
